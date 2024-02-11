f726ec53dc40a25b969ec1dbf4b4a68503640756
Javier  Restrepo Toro <jarestrepot@uqvirtual.edu.co>
2024-02-11 20:49:00 +0100

    ControlFlow - @For @Empty @switch

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,17 +1,45 @@
-import { Component, signal } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+type Grade = 'A' | 'B' | 'C';
+enum GradeEnum {
+  A = 'A',
+  B = 'B',
+  C = 'C',
+}
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './control-flow.component.html',
   styles: ``
 })
 export default class ControlFlowComponent {
 
-  public showContent = signal( false );
+  public showContent = signal(false);
+  public gradeChoose = new FormControl<Grade>('A');
+  public stateGrade = new FormControl<Grade>(GradeEnum.A);
+  public signalStateGrade = signal<Grade>(this.stateGrade.value ?? 'A');
+  public grade = signal<Grade | string>(this.gradeChoose.value ?? 'A');
 
-  public toggleContent(){
-    this.showContent.update( value => !value )
+  public frameworks = signal(['Angular', 'Vue', 'Svelte', 'Astro', 'Qwik', 'React']);
+  public frameworks2 = signal([]);
+
+
+
+  public gradeSelectEffect = effect(() => {
+    console.log(this.signalStateGrade())
+  });
+
+
+
+  public toggleContent() {
+    this.showContent.update(value => !value);
+  }
+
+  public selectChoise(value: Grade | string) {
+
+    this.grade.set(value.toLocaleUpperCase());
   }
 
 }
